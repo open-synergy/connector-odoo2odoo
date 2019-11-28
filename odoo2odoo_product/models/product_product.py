02db1791788df2605e55ef706c97b74cc328f172
Andhitia Rama <andhitia.r@gmail.com>
2019-11-29 06:01:09 +0700

    [8.0.2.0.0] odoo2odoo_product
    * Add manual binding feature
    * Temp disable test

--- odoo2odoo_product/models/product_product.py
+++ odoo2odoo_product/models/product_product.py
@@ -3,29 +3,32 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from openerp import models, fields
 
-from openerp.addons.odoo2odoo_backend.backend import odoo
-
-from ..consumer import OdooSyncExportMapper
-
 
 class ProductProduct(models.Model):
-    _name = 'product.product'
-    _inherit = ['product.product']
+    _name = "product.product"
+    _inherit = ["product.product"]
+    _no_export_field = "no_export_product"
 
     odoo_bind_ids = fields.One2many(
-        'odoo.product.product',
-        inverse_name='odoo_id',
+        "odoo.product.product",
+        inverse_name="odoo_id",
         string=u"Odoo Bindings",
         readonly=True)
 
+    no_export_product = fields.Boolean(
+        string="Exclude Product From Export",
+        default=True,
+    )
+
 
 class OdooProductProduct(models.Model):
-    _name = 'odoo.product.product'
-    _inherit = 'odoo.binding'
-    _inherits = {'product.product': 'odoo_id'}
+    _name = "odoo.product.product"
+    _inherit = "odoo.binding"
+    _inherits = {"product.product": "odoo_id"}
+    _description = "Product Binding"
 
     odoo_id = fields.Many2one(
-        'product.product',
+        "product.product",
         string=u"Product",
         required=True,
-        ondelete='cascade')
+        ondelete="cascade")
--- odoo2odoo_product/models/product_uom_categ.py
+++ odoo2odoo_product/models/product_uom_categ.py
@@ -13,64 +13,72 @@ logger = logging.getLogger(__name__)
 
 
 class ProductUomCateg(models.Model):
-    _name = 'product.uom.categ'
-    _inherit = ['product.uom.categ']
+    _name = "product.uom.categ"
+    _inherit = ["product.uom.categ"]
+    _no_export_field = "no_export"
 
     odoo_bind_ids = fields.One2many(
-        'odoo.product.uom.categ',
-        inverse_name='odoo_id',
-        string=u"Odoo Bindings",
-        readonly=True)
+        string="Odoo Bindings",
+        comodel_name="odoo.product.uom.categ",
+        inverse_name="odoo_id",
+        readonly=True
+    )
+    no_export = fields.Boolean(
+        string="Exclude From Export",
+        default=True,
+    )
 
 
 class OdooProductUomCateg(models.Model):
-    _name = 'odoo.product.uom.categ'
-    _inherit = 'odoo.binding'
-    _inherits = {'product.uom.categ': 'odoo_id'}
+    _name = "odoo.product.uom.categ"
+    _inherit = "odoo.binding"
+    _inherits = {"product.uom.categ": "odoo_id"}
+    _description = "Product UoM Category Binding"
 
     odoo_id = fields.Many2one(
-        'product.uom.categ',
-        string=u"UoM Category",
+        string="UoM Category",
+        comodel_name="product.uom.categ",
         required=True,
-        ondelete='cascade')
+        ondelete="cascade"
+    )
 
 
 @odoo(replacing=OdooSyncExporter)
 class OdooProductUomCategExporter(OdooSyncExporter):
-    _model_name = 'odoo.product.uom.categ'
+    _model_name = "odoo.product.uom.categ"
 
     def match_external_record(self, binding):
         """Try to match the local record with a remote one."""
         # Get all languages supported and ensure that 'en_US' is the last one
         # (last resort value if we do not find the corresponding translated
         # record, less error prones)
-        lang_codes = self.env['res.lang'].search([]).mapped('code')
-        lang_codes.pop(lang_codes.index('en_US'))
-        lang_codes.append('en_US')
+        lang_codes = self.env["res.lang"].search([]).mapped("code")
+        lang_codes.pop(lang_codes.index("en_US"))
+        lang_codes.append("en_US")
         # Try to find a remote record corresponding to the local one
         for lang_code in lang_codes:
             record_name = binding.with_context(lang=lang_code).name
             logger.info(
                 u"%s - Try to match the UoM category '%s' (lang='%s')...",
                 self.backend_record.name, record_name, lang_code)
-            self.backend_adapter.odoo_session.env.context['lang'] = lang_code
+            self.backend_adapter.odoo_session.env.context["lang"] = lang_code
             external_ids = self.backend_adapter.search(
-                [('name', '=', record_name)])
+                [("name", "=", record_name)])
             # Exclude record IDs already bound
             already_bound_external_ids = self.env[self._model_name].search(
-                [('external_odoo_id', 'in', external_ids)]).mapped(
-                    'external_odoo_id')
+                [("external_odoo_id", "in", external_ids)]).mapped(
+                    "external_odoo_id")
             external_ids = [id_ for id_ in external_ids
                             if id_ not in already_bound_external_ids]
             external_id = external_ids and external_ids[0] or False
             if external_id:
-                data = self.backend_adapter.read([external_id], ['name'])[0]
+                data = self.backend_adapter.read([external_id], ["name"])[0]
                 logger.info(
                     u"%s - UoM category '%s' (ID=%s) matches with "
                     u"the external UoM category '%s' (ID=%s)",
                     self.backend_record.name,
                     record_name, binding.odoo_id.id,
-                    data['name'], external_id)
+                    data["name"], external_id)
                 binding.with_context(
                     connector_no_export=True).external_odoo_id = external_id
                 break
